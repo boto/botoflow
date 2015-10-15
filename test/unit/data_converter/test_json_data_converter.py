ad1cb99dc661f564f8c5912fe48069d094042f1d
Chris Rose <chrisros@amazon.com>
2015-10-15 09:22:05 -0700

    Convert the JSON test to a pytest test

--- test/unit/data_converter/test_json_data_converter.py
+++ test/unit/data_converter/test_json_data_converter.py
@@ -1,10 +1,11 @@
 import copy
-import unittest
 import zlib
 import six
 
 from decimal import Decimal
 from collections import namedtuple, OrderedDict
+
+import pytest
 
 from awsflow import WorkflowDefinition, execute
 from awsflow.data_converter import JSONDataConverter
@@ -60,159 +61,146 @@ class MyCustomException(Exception):
         self.other = other
 
 
-class TestJSONDataConverter(unittest.TestCase):
+@pytest.fixture
+def serde():
+    return JSONDataConverter()
 
-    def setUp(self):
-        self.serde = JSONDataConverter()
 
-    def dumps_loads(self, obj):
-        return self.serde.loads(self.serde.dumps(obj))
+def dumps_loads(serde, obj):
+    return serde.loads(serde.dumps(obj))
 
-    def test_tuples(self):
-        self.assertEqual('{"__tuple":[]}', self.serde.dumps(tuple()))
-        self.assertEqual(tuple, type(self.dumps_loads(tuple())))
 
-        nested_tuple = tuple([(1,2), (3, 4)])
-        self.assertEqual(nested_tuple, self.dumps_loads(nested_tuple))
+def test_tuples(serde):
+    assert serde.dumps(tuple()) == '{"__tuple":[]}'
+    assert type(dumps_loads(serde, tuple())) == tuple
 
-    def test_sets(self):
-        self.assertEqual('{"__set":[1,2,3]}',
-                         self.serde.dumps(set([1,2,3])))
-        self.assertEqual(set, type(self.dumps_loads(set())))
+    nested_tuple = tuple([(1,2), (3, 4)])
+    assert dumps_loads(serde, nested_tuple) == nested_tuple
 
-        nested_set = set([frozenset([1, 2]), frozenset([3, 4])])
-        self.assertEqual(nested_set, self.dumps_loads(nested_set))
+def test_sets(serde):
+    assert serde.dumps(set([1,2,3])) == '{"__set":[1,2,3]}'
+    assert type(dumps_loads(serde, set())) == set
 
-    def test_frozensets(self):
-        self.assertEqual('{"__frozenset":[1,2,3]}',
-                         self.serde.dumps(frozenset([1,2,3])))
-        self.assertEqual(frozenset, type(self.dumps_loads(frozenset())))
+    nested_set = set([frozenset([1, 2]), frozenset([3, 4])])
+    assert dumps_loads(serde, nested_set) == nested_set
 
-        nested_frozenset = frozenset([frozenset([1, 2]), frozenset([3, 4])])
-        self.assertEqual(nested_frozenset, self.dumps_loads(nested_frozenset))
+def test_frozensets(serde):
+    assert serde.dumps(frozenset([1,2,3])) == '{"__frozenset":[1,2,3]}'
+    assert type(dumps_loads(serde, frozenset())) == frozenset
 
-    def test_namedtuples(self):
-        named_tuple = NamedTuple(1, 'a')
-        nested_ntuple = NamedTuple(1, NamedTuple(2, 'b'))
+    nested_frozenset = frozenset([frozenset([1, 2]), frozenset([3, 4])])
+    assert dumps_loads(serde, nested_frozenset) == nested_frozenset
 
-        self.assertEqual('{"__namedtuple":["test_json_data_converter:NamedTuple"'
-                         ',[1,"a"]]}',
-                         self.serde.dumps(named_tuple))
+def test_namedtuples(serde):
+    named_tuple = NamedTuple(1, 'a')
+    nested_ntuple = NamedTuple(1, NamedTuple(2, 'b'))
 
-        self.assertEqual(NamedTuple, type(self.dumps_loads(named_tuple)))
-        self.assertEqual(named_tuple, self.dumps_loads(named_tuple))
-        self.assertEqual(nested_ntuple, self.dumps_loads(nested_ntuple))
+    assert serde.dumps(named_tuple) == \
+        '{"__namedtuple":["test_json_data_converter:NamedTuple",[1,"a"]]}'
 
-    def test_decimal(self):
-        number = Decimal(1.1)
-        assert number == self.dumps_loads(number)
 
-    def test_decimal_inf(self):
-        inf = Decimal('inf')
-        assert inf == self.dumps_loads(inf)
+    assert type(dumps_loads(serde, named_tuple)) == NamedTuple
+    assert dumps_loads(serde, named_tuple) == named_tuple
+    assert dumps_loads(serde, nested_ntuple) == nested_ntuple
 
-    def test_objects(self):
+def test_decimal(serde):
+    number = Decimal(1.1)
+    assert number == dumps_loads(serde, number)
 
-        self.assertEqual('test', self.dumps_loads(SimpleObj('test')).input)
+def test_decimal_inf(serde):
+    inf = Decimal('inf')
+    assert inf == dumps_loads(serde, inf)
 
-    def test_class(self):
+def test_objects(serde):
+    assert dumps_loads(serde, SimpleObj('test')).input == 'test'
 
-        self.assertEqual(SimpleObj, self.dumps_loads(SimpleObj))
+def test_class(serde):
+    assert dumps_loads(serde, SimpleObj) == SimpleObj
 
-    def test_string(self):
-        self.assertEqual('test', self.dumps_loads('test'))
+def test_string(serde):
+    assert dumps_loads(serde, 'test') == 'test'
 
-    def test_unicode(self):
-        ustring = six.unichr(40960) + u'abcd' + six.unichr(1972)
-        self.assertEqual(ustring, self.dumps_loads(ustring))
+def test_unicode(serde):
+    ustring = six.unichr(40960) + u'abcd' + six.unichr(1972)
+    assert dumps_loads(serde, ustring) == ustring
 
-    def test_zlib(self):
-        # This test is really about ensuring that binary data isn't corrupted
-        data = six.b('compress me')
-        compressed = zlib.compress(data)
-        self.assertEqual(data, zlib.decompress(self.dumps_loads(compressed)))
+def test_zlib(serde):
+    # This test is really about ensuring that binary data isn't corrupted
+    data = six.b('compress me')
+    compressed = zlib.compress(data)
+    assert zlib.decompress(dumps_loads(serde, compressed)) == data
 
-    def test_states_objects(self):
-        self.assertFalse('present' in \
-                         self.serde.dumps(StateObj('test', 'present')))
+def test_states_objects(serde):
+    assert 'present' not in serde.dumps(StateObj('test', 'present'))
 
-        self.assertEqual('blah',
-                         self.dumps_loads(StateObj('test', 'present')).input2)
+    assert dumps_loads(serde, StateObj('test', 'present')).input2 == 'blah'
 
-    def test_serialize_self(self):
-        self.dumps_loads(self.serde)
+def test_serialize_self(serde):
+    dumps_loads(serde, serde)
 
-    def test_dict(self):
-        self.assertEqual('{"spam":"eggs"}', self.serde.dumps({'spam': 'eggs'}))
+def test_dict(serde):
+    assert serde.dumps({'spam': 'eggs'}) == '{"spam":"eggs"}'
 
-    def test_dict_subclass(self):
-        subdct = DictSubclass()
-        subdct['testval'] = 'test'
-        subdct.attr = 'testattr'
+def test_dict_subclass(serde):
+    subdct = DictSubclass()
+    subdct['testval'] = 'test'
+    subdct.attr = 'testattr'
 
-        result = self.dumps_loads(subdct)
-        self.assertEqual('test', result.myval())
-        self.assertEqual('testattr', result.attr)
-        self.assertEqual(DictSubclass, type(result))
+    result = dumps_loads(serde, subdct)
+    assert result.myval() == 'test'
+    assert result.attr == 'testattr'
+    assert type(result) == DictSubclass
 
-    def test_list_subclass(self):
-        sublst = ListSubclass()
-        sublst.extend(('testone', 'testtwo'))
-        sublst.attr = 'testattr'
+def test_list_subclass(serde):
+    sublst = ListSubclass()
+    sublst.extend(('testone', 'testtwo'))
+    sublst.attr = 'testattr'
 
-        result = self.dumps_loads(sublst)
-        self.assertEqual('testtwo', result.secondval())
-        self.assertEqual('testattr', result.attr)
-        self.assertEqual(ListSubclass, type(result))
+    result = dumps_loads(serde, sublst)
+    assert result.secondval() == 'testtwo'
+    assert result.attr == 'testattr'
+    assert type(result) == ListSubclass
 
-    def test_nested_subclass(self):
-        subdct = DictSubclass()
-        subdct['testval'] = 'test'
-        subdct.sublst = ListSubclass()
-        subdct.sublst.extend(('testone', 'testtwo'))
+def test_nested_subclass(serde):
+    subdct = DictSubclass()
+    subdct['testval'] = 'test'
+    subdct.sublst = ListSubclass()
+    subdct.sublst.extend(('testone', 'testtwo'))
 
-        result = self.dumps_loads(subdct)
-        self.assertEqual('test', result.myval())
-        self.assertEqual(DictSubclass.attr, result.attr)
-        self.assertEqual(DictSubclass, type(result))
+    result = dumps_loads(serde, subdct)
+    assert result.myval() == 'test'
+    assert result.attr == DictSubclass.attr
+    assert type(result) == DictSubclass
 
-        self.assertEqual('testtwo', result.sublst.secondval())
-        self.assertEqual(ListSubclass.attr, result.sublst.attr)
-        self.assertEqual(ListSubclass, type(result.sublst))
+    assert result.sublst.secondval() == 'testtwo'
+    assert result.sublst.attr == ListSubclass.attr
+    assert type(result.sublst) == ListSubclass
 
-    def test_ordereddict(self):
-        inner_dct = OrderedDict(((3, 'c'), (4, 'd')))
-        dct = OrderedDict(((1, 'a'), (2, inner_dct)))
+def test_ordereddict(serde):
+    inner_dct = OrderedDict(((3, 'c'), (4, 'd')))
+    dct = OrderedDict(((1, 'a'), (2, inner_dct)))
 
-        self.assertEqual(dct, self.dumps_loads(dct))
-        self.assertEqual('{"__ordereddict":[[1,"a"],[2,'
-                         '{"__ordereddict":[[3,"c"],[4,"d"]]}]]}',
-                         self.serde.dumps(dct))
+    assert dumps_loads(serde, dct) == dct
+    assert serde.dumps(dct) == '{"__ordereddict":[[1,"a"],[2,{"__ordereddict":[[3,"c"],[4,"d"]]}]]}'
 
-    def test_workflow_definition(self):
-        self.assertEqual(MyWorkflowDefinition,
-                         self.serde.loads(
-                             self.serde.dumps(MyWorkflowDefinition)))
+def test_workflow_definition(serde):
+    assert serde.loads(serde.dumps(MyWorkflowDefinition)) == MyWorkflowDefinition
 
-    def test_exceptions(self):
-        e = MyCustomException(u'some error message', 'someparam')
-        r = self.dumps_loads(e)
+def test_exceptions(serde):
+    e = MyCustomException(u'some error message', 'someparam')
+    r = dumps_loads(serde, e)
 
-        self.assertEqual(str(e), str(r))
-        self.assertEqual(e.other, r.other)
+    assert str(r) == str(e)
+    assert r.other == e.other
 
-    def test_unimportable_exception(self):
-        raw = '{"__obj":["unknown_module:MyUnknownException",{"other":"someparam"}],"__exc":[["some error message"],"some error message"]}'
-        r = self.serde.loads(raw)
-        self.assertTrue(isinstance(r, ImportError))
-        self.assertEqual(r.message, 'unknown_module.MyUnknownException: some error message')
-        self.assertEqual(r.other, 'someparam')
+def test_unimportable_exception(serde):
+    raw = '{"__obj":["unknown_module:MyUnknownException",{"other":"someparam"}],"__exc":[["some error message"],"some error message"]}'
+    r = serde.loads(raw)
+    assert isinstance(r, ImportError)
+    assert 'unknown_module.MyUnknownException: some error message' == r.message
+    assert 'someparam' == r.other
 
-    def test_unimportable_object(self):
-        raw = '{"__obj":["unknown_module:MyUnknownObject",{"other":"someparam"}]}'
-        with self.assertRaises(ImportError):
-            self.serde.loads(raw)
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_unimportable_object(serde):
+    raw = '{"__obj":["unknown_module:MyUnknownObject",{"other":"someparam"}]}'
+    with pytest.raises(ImportError):
+        serde.loads(raw)
