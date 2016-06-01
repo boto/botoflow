17b08228bbeaef9c5a63daf278b96e770ab44722
Darjus Loktevic <darjus@gmail.com>
2016-06-01 14:07:07 -0700

    Initial setup.py and setup.cfg setup

--- setup.py
+++ setup.py
@@ -1,31 +1,38 @@
 from setuptools import setup, find_packages
 
+import botoflow
 
 requires = ['botocore>=1.1.10',
-            'six>=1.2.0']
+            'six>=1.2.0',
+            'dill>=0.2']
 
 args = dict(
     name='botoflow',
-    version='1.0-preview',
-    author='Amazon.com',
-    description="The The botoflow is a programming framework that "
-    "works together with Amazon Simple Workflow Service (Amazon SWF) to "
-    "help developers build asynchronous and distributed applications",
-    packages=find_packages('lib'),
+    version=botoflow.__version__,
+    author='Amazon.com, Darjus Loktevic',
+    author_email='darjus@gmail.com',
+    description="Botoflow is an asynchronous framework for Amazon SWF that helps you "
+                "build SWF applications using Python",
+    long_description=open('README.rst').read(),
+    url='https://github.com/boto/botoflow',
+    packages=find_packages(exclude=['test*']),
     scripts=[],
     cmdclass={},
     install_requires=requires,
-    license=open("LICENSE.txt").read(),
-    package_dir={'': 'lib'},
+    license="Apache License 2.0",
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
     )
-
-
-# setup sphinx command
-try:
-    from sphinx.setup_command import BuildDoc
-    args['cmdclass']['build_sphinx'] = BuildDoc
-except ImportError:
-    pass
 
 setup(**args)
 
